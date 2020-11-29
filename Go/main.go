da22b383412fa9c84e15f83762118a08d35e031a
Apostrov <amir.subaev@gmail.com>
2020-11-29 19:37:50 +0300

    Complete go main bencmark file

---
+++ Go/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"nbody"
+	"runtime"
+	"time"
+)
+
+func main() {
+	run("Nbody", 10, func() {
+		nbody.RunBenchmark(50000000, false)
+	})
+}
+
+func run(name string, iterations int, action func()) {
+	fmt.Printf("Running benchmark %s for %d iterations... ", name, iterations)
+
+	// Perform garbage collection.
+	runtime.GC()
+
+	start := time.Now()
+	for i := 0; i < iterations; i++ {
+		fmt.Printf("%d ", i)
+		action()
+	}
+	duration := time.Since(start)
+	fmt.Println()
+	fmt.Printf("Elapsed time %d ms.\n", duration.Milliseconds())
+}
--- Go/src/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"nbody"
-)
-
-func main() {
-	nbody.RunBenchmark(5000, true)
-}
